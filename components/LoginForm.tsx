55d7a99d4e0838acc00265d7f5d5b3ad9508f394
shmil123 <arielkeren2007@gmail.com>
2022-12-10 17:16:41 +0200

    Change login page styles

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,8 +1,6 @@
 import { auth, signInWithGoogle } from "../firebaseConfig";
-import Title from "./Title";
 import { FcGoogle } from "react-icons/fc";
 import { useState } from "react";
-import styles from "../styles/LoginForm.module.css";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -46,54 +44,68 @@ const LoginForm: React.FC = () => {
   return (
     <>
       {auth.currentUser === null ? (
-        <div className={styles.container}>
-          <form className={styles.form}>
-            <div className={styles.fieldContainer}>
-              <label htmlFor="email" className={styles.label}>
-                Email
-              </label>
+        <form className="flex flex-col items-center">
+          <div className="w-1/2 flex flex-col items-center gap-3">
+            <div className="flex flex-col items-center w-full">
+              <div className="flex justify-start w-4/5">
+                <label
+                  htmlFor="email"
+                  className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+                >
+                  Email
+                </label>
+              </div>
               <input
                 type="email"
                 name="email"
                 id="email"
                 value={email}
                 onChange={changeEmail}
-                className={styles.input}
+                className="w-4/5 text-3xl p-3 rounded-md outline-none focus:bg-gray-100"
               />
             </div>
-            <div className={styles.fieldContainer}>
-              <label htmlFor="password" className={styles.label}>
-                Password
-              </label>
+            <div className="flex flex-col items-center w-full">
+              <div className="flex justify-start w-4/5">
+                <label
+                  htmlFor="password"
+                  className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+                >
+                  Password
+                </label>
+              </div>
               <input
                 type="password"
                 name="password"
                 id="password"
                 value={password}
                 onChange={changePassword}
-                className={styles.input}
+                className="w-4/5 text-3xl p-3 rounded-md outline-none focus:bg-gray-100"
               />
             </div>
             <input
               type="submit"
               value="Log in"
               onClick={emailAndPasswordLogin}
-              className={styles.logInButton}
+              className="my-5 bg-blue-600 text-white py-3 px-12 rounded-lg font-bold text-3xl cursor-pointer hover:bg-blue-700 transition-colors"
             />
-            <p className={styles.signUpText}>
-              Don&apos;t have an account? <Link href="/sign-up">Sign up</Link>
-            </p>
-            <Title title="OR" />
-            <button
-              onClick={googleLogin}
-              className={styles.continueWithGoogleButton}
-            >
-              <FcGoogle className={styles.googleIcon} /> Continue with Google
-            </button>
-          </form>
-        </div>
+          </div>
+          <p className="text-white font-bold text-xl">
+            Don&apos;t have an account?{" "}
+            <Link href="/sign-up" className="text-blue-600 hover:underline">
+              Sign up
+            </Link>
+          </p>
+          <button
+            onClick={googleLogin}
+            className="mt-10 flex items-center text-2xl text-white shadow-xl py-5 px-12 rounded-lg bg-slate-800 hover:bg-slate-700 transition-colors duration-300"
+          >
+            <FcGoogle className="text-5xl mr-2" /> Continue with Google
+          </button>
+        </form>
       ) : (
-        <p className={styles.loggedInText}>You&apos;re already logged in...</p>
+        <p className="text-center text-white text-3xl font-bold">
+          You&apos;re already logged in...
+        </p>
       )}
     </>
   );
