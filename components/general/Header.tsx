848a0e10dc4f092ab5282c843ae7b4ed74e58e51
shmil123 <arielkeren2007@gmail.com>
2023-01-07 21:05:02 +0200

    Change the header

--- components/general/Header.tsx
+++ components/general/Header.tsx
@@ -3,21 +3,20 @@ import { auth } from "../../firebaseConfig";
 import { FiLogIn } from "react-icons/fi";
 import { RiUser3Fill } from "react-icons/ri";
 import Image from "next/image";
+import Title from "./Title";
 
 const Header: React.FC = () => (
-  <div className="flex justify-between items-center p-7 bg-white shadow-xl">
-    <Link
-      href="/"
-      className="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 p-2 hover:text-cyan-400 transition-colors duration-500"
-    >
+  <div className="flex justify-between items-center p-7">
+    <Link href="/" className="flex items-end font-medium text-3xl">
       <Image
         src="/Logo.svg"
         alt="Coding Classroom"
-        width="150"
-        height="100"
+        width="55"
+        height="50"
         priority
-        className="h-16 w-36"
+        className="mr-2"
       />
+      SchoolBit
     </Link>
     {auth.currentUser?.displayName ? (
       <div className="flex items-center">
