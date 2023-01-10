8cab7c459a9bbe0c088997a8b4720f0b01944d9d
shmil123 <arielkeren2007@gmail.com>
2023-01-10 07:33:48 +0200

    Fix font not loading in some page requests

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ import { Poppins } from "@next/font/google";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
 });
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
