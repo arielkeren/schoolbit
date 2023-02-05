8856b9a3b4aa05c57c7c2b6662b630d80a27f8ef
shmil123 <arielkeren2007@gmail.com>
2023-02-06 00:18:25 +0200

    Make the create classroom page responsive

--- components/create-classroom/CreateClassroomForm.tsx
+++ components/create-classroom/CreateClassroomForm.tsx
@@ -6,7 +6,7 @@ import {
   setDoc,
 } from "firebase/firestore";
 import { useState } from "react";
-import { auth, database } from "../../firebaseConfig";
+import { database } from "../../firebaseConfig";
 import { IClassroomData, IClassroom } from "../../types/types";
 import { HexColorPicker } from "react-colorful";
 import { useRouter } from "next/router";
@@ -94,68 +94,63 @@ const CreateClassroomForm: React.FC = () => {
   };
 
   return (
-    <>
-      {auth.currentUser ? (
-        <form className="flex justify-center mb-10">
-          <div className="w-1/2 flex flex-col items-center gap-3">
-            <div className="flex flex-col items-center w-full">
-              <div className="flex justify-start w-4/5">
-                <label htmlFor="name" className="text-2xl font-bold">
-                  Name
-                </label>
-              </div>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                value={name}
-                onChange={changeName}
-                autoFocus
-                className="w-4/5 text-3xl p-3 rounded-md outline-none bg-gray-100 focus:bg-gray-200 transition-colors"
-              />
-            </div>
-
-            <div className="flex flex-col items-center w-full">
-              <div className="flex justify-start w-4/5">
-                <label htmlFor="description" className="text-2xl font-bold">
-                  Description
-                </label>
-              </div>
-              <input
-                type="text"
-                name="description"
-                id="description"
-                value={description}
-                onChange={changeDescription}
-                className="w-4/5 text-3xl p-3 rounded-md outline-none bg-gray-100 focus:bg-gray-200 transition-colors"
-              />
-            </div>
-
-            <div className="flex flex-col items-center w-full m-5 gap-2">
-              <div className="flex justify-center w-4/5">
-                <h3 className="text-2xl font-bold">Choose Color</h3>
-              </div>
-              <HexColorPicker
-                color={color}
-                onChange={setColor}
-                className="shadow-xl"
-              />
-            </div>
-
-            <input
-              type="submit"
-              value="Create"
-              onClick={preventDefault}
-              className="mt-5 bg-gray-900 text-white py-3 px-12 rounded-lg font-bold text-3xl uppercase cursor-pointer hover:bg-gray-800 transition-colors"
-            />
+    <form className="flex justify-center mb-10">
+      <div className="w-full flex flex-col items-center gap-3 lg:w-11/12">
+        <div className="flex flex-col items-center w-full">
+          <div className="flex justify-start w-4/5">
+            <label htmlFor="name" className="text-lg font-bold md:text-xl">
+              Name
+            </label>
           </div>
-        </form>
-      ) : (
-        <p className="text-3xl font-bold text-center">
-          Log in to create a classroom
-        </p>
-      )}
-    </>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={changeName}
+            autoFocus
+            className="w-4/5 p-3 rounded-md outline-none bg-gray-100 focus:bg-gray-200 transition-colors md:text-lg"
+          />
+        </div>
+
+        <div className="flex flex-col items-center w-full">
+          <div className="flex justify-start w-4/5">
+            <label
+              htmlFor="description"
+              className="text-lg font-bold md:text-xl"
+            >
+              Description
+            </label>
+          </div>
+          <input
+            type="text"
+            name="description"
+            id="description"
+            value={description}
+            onChange={changeDescription}
+            className="w-4/5 p-3 rounded-md outline-none bg-gray-100 focus:bg-gray-200 transition-colors md:text-lg"
+          />
+        </div>
+
+        <div className="flex flex-col items-center w-full m-5 gap-2">
+          <div className="flex justify-center w-4/5">
+            <h3 className="text-xl font-extrabold md:text-2xl">Banner Color</h3>
+          </div>
+          <HexColorPicker
+            color={color}
+            onChange={setColor}
+            className="shadow-xl"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Create"
+          onClick={preventDefault}
+          className="mt-5 bg-gray-900 text-white py-3 px-12 rounded-lg font-bold text-3xl uppercase cursor-pointer hover:bg-gray-800 transition-colors"
+        />
+      </div>
+    </form>
   );
 };
 
