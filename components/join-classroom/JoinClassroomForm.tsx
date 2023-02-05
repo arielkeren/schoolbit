f40e338d49dbf6598d0ba31a1ad03f52db703058
shmil123 <arielkeren2007@gmail.com>
2023-02-06 00:09:49 +0200

    Make the join classroom page responsive

--- components/join-classroom/JoinClassroomForm.tsx
+++ components/join-classroom/JoinClassroomForm.tsx
@@ -58,41 +58,36 @@ const JoinClassroomForm: React.FC = () => {
   };
 
   return (
-    <>
-      {user ? (
-        <form className="flex justify-center">
-          <div className="w-1/2 flex flex-col items-center gap-3">
-            <div className="flex flex-col items-center w-full">
-              <div className="flex justify-start w-4/5">
-                <label htmlFor="classroom-code" className="text-2xl font-bold">
-                  Classroom Code
-                </label>
-              </div>
-              <input
-                type="text"
-                name="classroom-code"
-                id="classroom-code"
-                value={classroomCode}
-                onChange={changeClassroomCode}
-                onKeyDown={detectEnterKey}
-                autoFocus
-                className="w-4/5 text-3xl p-3 rounded-md outline-none bg-gray-100 focus:bg-gray-200 transition-colors"
-              />
-            </div>
-            <input
-              type="submit"
-              value="Send request"
-              onClick={preventDefault}
-              className="mt-5 bg-gray-900 text-white py-3 px-12 rounded-lg font-bold text-3xl uppercase cursor-pointer hover:bg-gray-800 transition-colors"
-            />
+    <form className="flex justify-center">
+      <div className="w-full flex flex-col items-center gap-3 lg:w-4/5">
+        <div className="flex flex-col items-center w-full">
+          <div className="flex justify-start w-4/5">
+            <label
+              htmlFor="classroom-code"
+              className="text-lg font-bold sm:text-xl"
+            >
+              Classroom Code
+            </label>
           </div>
-        </form>
-      ) : (
-        <p className="text-3xl font-bold text-center">
-          Log in to join a classroom
-        </p>
-      )}
-    </>
+          <input
+            type="text"
+            name="classroom-code"
+            id="classroom-code"
+            value={classroomCode}
+            onChange={changeClassroomCode}
+            onKeyDown={detectEnterKey}
+            autoFocus
+            className="w-4/5 p-3 rounded-md outline-none bg-gray-100 focus:bg-gray-200 transition-colors sm:text-lg"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Send Request"
+          onClick={preventDefault}
+          className="mt-5 bg-gray-900 text-white py-3 px-4 rounded font-bold text-2xl uppercase cursor-pointer hover:bg-gray-800 transition-colors lg:text-3xl lg:px-10"
+        />
+      </div>
+    </form>
   );
 };
 
