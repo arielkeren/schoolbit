016e1f385fd1333aae81742970a4995006090de8
shmil123 <arielkeren2007@gmail.com>
2023-02-13 19:29:37 +0200

    Add the programming language section to the edit assignment page

--- components/edit/EditAssignmentForm.tsx
+++ components/edit/EditAssignmentForm.tsx
@@ -5,6 +5,8 @@ import { IAssignment } from "../../types/types";
 import { useRouter } from "next/router";
 import Calendar from "react-calendar";
 import useAppContext from "../../hooks/useAppContext";
+import ProgrammingLanguagePicker from "../general/ProgrammingLanguagePicker";
+import { TfiLock, TfiUnlock } from "react-icons/tfi";
 
 const EditAssignmentForm: React.FC = () => {
   const { classroom, changeClassroom } = useAppContext();
@@ -18,6 +20,8 @@ const EditAssignmentForm: React.FC = () => {
   const [name, setName] = useState("");
   const [question, setQuestion] = useState("");
   const [date, setDate] = useState(new Date());
+  const [language, setLanguage] = useState("javascript");
+  const [isLanguageLocked, setIsLanguageLocked] = useState(false);
 
   useEffect(() => {
     const assignment = classroom?.assignments.find(
@@ -35,6 +39,8 @@ const EditAssignmentForm: React.FC = () => {
     setName(assignment?.name ?? "");
     setQuestion(assignment?.question ?? "");
     setDate(dateFormat);
+    setLanguage(assignment?.language ?? "javascript");
+    setIsLanguageLocked(assignment?.isLanguageLocked ?? false);
   }, [classroom, assignmentID]);
 
   const changeName = (event: React.ChangeEvent<HTMLInputElement>) =>
@@ -43,8 +49,21 @@ const EditAssignmentForm: React.FC = () => {
   const changeQuestion = (event: React.ChangeEvent<HTMLTextAreaElement>) =>
     setQuestion(event.target.value);
 
+  const changeLanguage = (event: React.ChangeEvent<HTMLSelectElement>) =>
+    setLanguage(event.target.value);
+
+  const lockLanguage = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsLanguageLocked(true);
+  };
+
+  const unlockLanguage = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsLanguageLocked(false);
+  };
+
   const editAssignment = async () => {
-    if (!name || !question || !date || !classroom) return;
+    if (!classroom) return;
 
     const noUnnecessarySpacesName = name.trim().replace(/\s{2,}/g, " ");
     if (noUnnecessarySpacesName === "" || question.replaceAll(" ", "") === "")
@@ -63,6 +82,8 @@ const EditAssignmentForm: React.FC = () => {
       name: noUnnecessarySpacesName,
       question,
       until: shortenedDate,
+      language,
+      isLanguageLocked,
       answers: [],
       id: assignmentID,
     };
@@ -123,7 +144,33 @@ const EditAssignmentForm: React.FC = () => {
           />
         </div>
 
+        <h2 className="text-xl font-bold">Language</h2>
+
+        <div className="flex items-center gap-1">
+          {isLanguageLocked ? (
+            <button
+              onClick={unlockLanguage}
+              className="hover:bg-gray-100 p-1 rounded-full"
+            >
+              <TfiLock className="text-xl text-red-600" />
+            </button>
+          ) : (
+            <button
+              onClick={lockLanguage}
+              className="hover:bg-gray-100 p-1 rounded-full"
+            >
+              <TfiUnlock className="text-xl text-green-600" />
+            </button>
+          )}
+
+          <ProgrammingLanguagePicker
+            languageID={language}
+            changeLanguage={changeLanguage}
+          />
+        </div>
+
         <h2 className="text-xl font-bold">Due Date</h2>
+
         <Calendar
           value={date}
           onChange={setDate}
